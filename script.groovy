3212b1ba0364c06623b6858b14d8b2bd2d03d289
Ilia Falaleev <43509464+ilka95@users.noreply.github.com>
2022-03-19 20:47:59 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ pipeline {
     stage('Run Docker') {
       steps {
         sh(script:'''
-          docker run --name mynginx -p 80:9889 -d nginx;
+          sudo docker run --name mynginx -p 80:9889 -d nginx;
         ''')
       }
     }
