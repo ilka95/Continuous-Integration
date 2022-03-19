a1de94ca0874153fbc0960218410b4a74f3f90d1
Ilia Falaleev <43509464+ilka95@users.noreply.github.com>
2022-03-20 00:00:10 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ pipeline {
     stage('Run Docker') {
       steps {
         sh(script:''' 
-          sudo -S docker run --name mynginx -p 9889:80 -d -v /$WORKSPACE/index/:/usr/share/nginx/html/ nginx < /tmp/pass
+          sudo -S docker run --name mynginx -p 9889:80 -d -v $WORKSPACE/index/:/usr/share/nginx/html/ nginx < /tmp/pass
         ''')
       }
     }
@@ -18,9 +18,28 @@ pipeline {
             returnStdout: true
           ).trim()
           if("${status}"=="200") {
-            println("Nginx is UP correctly")
+            println("Nginx is UP correctly!")
           } else {
             println("Nginx is UP incorrectly! Status = ${status}")
+          }
+        }
+      }
+      stage('Check md5sum') {
+      steps {
+        script {
+          def sum = sh (
+            script: 'cat index/index.html | md5sum',
+            returnStdout: true
+          ).trim()
+          def result = sh (
+            script: 'curl localhost:9889 | grep ${sum}',
+            returnStdout: true
+          ).trim()
+          
+          if("${result}" != null) {
+            println("Index.html is correct!")
+          } else {
+            println("Index.html is incorrect!")
           }
         }
       }
