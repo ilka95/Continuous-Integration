12dc0ba514a3b02f2a4a280801049d67a00c0155
Ilia Falaleev <43509464+ilka95@users.noreply.github.com>
2022-03-19 23:22:44 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,17 +3,20 @@ pipeline {
   agent any
   stages {
     stage('Run Docker') {
-        sh 'sudo -S docker run --name mynginx -p 9889:80 -d -v /$WORKSPACE/index/:/usr/share/nginx/html/ nginx < /tmp/pass'
+      steps {
+        sh(script:''' 
+          sudo -S docker run --name mynginx -p 9889:80 -d -v /$WORKSPACE/index/:/usr/share/nginx/html/ nginx < /tmp/pass
+        ''')
+      }
     }
     stage('Check Nginx Status') {
+      steps {
         sh 'curl -s -o /dev/null -w "%{http_code}" localhost:9889 > status.txt'
-        status = sh (
-          script: 'cat status.txt',
-          returnStdout:true
-        ).trim()
-        if("${status}"=="200") {
-          println("Nginx is UP correctly")
+        //status = sh (script: 'cat status.txt')
+        //if("${status}"=="200") {
+        //  println("Nginx is UP correctly")
         }
+      }
     }
   }
 }
