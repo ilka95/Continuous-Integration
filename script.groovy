6a9b12d6ef765423e14d45400d1dfc20d4e5e3c8
Ilia Falaleev <43509464+ilka95@users.noreply.github.com>
2022-03-19 21:59:44 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ pipeline {
         sh(script:'''
           whoami
           echo '123' > passwor.secret
-          sudo -S docker run --name mynginx -p 80:9889 -d nginx < passwor.secret;
+          sudo -S docker run --name mynginx -p 9889:80 -d nginx < passwor.secret;
         ''')
       }
     }
