26486f9e2ecea019a3594495d8ab40426201ff19
Ilia Falaleev <43509464+ilka95@users.noreply.github.com>
2022-03-19 22:07:29 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,9 +5,9 @@ pipeline {
     stage('Run Docker') {
       steps {
         sh(script:'''
-          whoami
-          echo '123' > passwor.secret
-          sudo -S docker run --name mynginx -p 9889:80 -d nginx < passwor.secret;
+          sudo -S docker stop mynginx < /tmp/pass
+          sudo -S docker rm mynginx < /tmp/pass    
+          sudo -S docker run --name mynginx -p 9889:80 -d nginx < /tmp/pass
         ''')
       }
     }
