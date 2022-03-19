21d1b8283dd6a01a362d0da950b67c9c6eee5491
Ilia Falaleev <43509464+ilka95@users.noreply.github.com>
2022-03-19 22:49:06 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ pipeline {
         sh(script:'''
           sudo -S docker stop mynginx < /tmp/pass
           sudo -S docker rm mynginx < /tmp/pass    
-          sudo -S docker run --name mynginx -p 9889:80 -d -v index/:/usr/share/nginx/html/ nginx < /tmp/pass
+          sudo -S docker run --name mynginx -p 9889:80 -d -v /$WORKSPACE/index/:/usr/share/nginx/html/ nginx < /tmp/pass
         ''')
       }
     }
